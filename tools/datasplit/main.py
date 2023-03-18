02d9e0a9bd7b0b8a5761298a0cdf26f210856872
KuoLiChia_windows <richardkuo1999@gmail.com>
2023-03-18 19:34:09 +0800

    can move txt label

--- tools/datasplit/main.py
+++ tools/datasplit/main.py
@@ -4,9 +4,14 @@ import shutil
 
 
 Proportion = [6,1]
+source_path = Path('datasets')
+save_path = Path('result')
+
+
+
+
+
 if __name__ == "__main__":
-    source_path = Path('datasets')
-    save_path = Path('result')
     (save_path / 'images' / 'train').mkdir(parents=True)
     (save_path / 'labels' / 'train').mkdir(parents=True)
     (save_path / 'images' / 'val').mkdir(parents=True)
@@ -21,23 +26,28 @@ if __name__ == "__main__":
           (save_path / 'labels' / 'train' / dataclass.name).mkdir(parents=True)
           (save_path / 'images' / 'val' / dataclass.name).mkdir(parents=True)
           (save_path / 'labels' / 'val' / dataclass.name).mkdir(parents=True)
+          
+
         # rename the label file
         for original in (dataclass/'label').glob('*.bmp'):
           new = original.parent / ((original.stem).split('_')[0]+'.bmp')
           shutil.move(original , new) # image
 
+
+        # split data
         image_list = [p.stem for p in (dataclass).glob('*.bmp')]
         k = len(image_list) // sum(Proportion) * Proportion[0]
         train_image_list = random.sample(image_list, k=k)
         print(f'{dataclass.name}:{k}')
 
+        # copy file
         for data in dataclass.glob('*.bmp'):
-            move_to = ('train' if data.stem in train_image_list else 'val')
-            if flag: 
-                shutil.copy(data, save_path /'images' / move_to / dataclass.name /data.name)
-                label_path =  dataclass / 'label' / data.name
-                shutil.copy(label_path, save_path / 'labels' / move_to / dataclass.name / data.name)
-            else:
-                shutil.copy(data, save_path /'images' / move_to /data.name)
-                label_path =  dataclass / 'label' / data.name
-                shutil.copy(label_path, save_path / 'labels' / move_to /data.name)
+            move_to = Path('train' if data.stem in train_image_list else 'val')
+            path_type = move_to / dataclass.name if flag else move_to
+
+            shutil.copy(data, save_path /'images' / path_type /data.name)
+            label_path =  dataclass / 'label' / data.name
+            shutil.copy(label_path, save_path / 'labels' / path_type / data.name)
+            label_path = Path(f'{str(label_path).split(".")[0]}.txt')
+            if label_path.exists():
+              shutil.copy(label_path, save_path / 'labels' / path_type / (str(data.stem)+'.txt'))
