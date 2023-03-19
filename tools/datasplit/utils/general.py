baf91a1722a7592cd0bbc3b84c99f22c7b14289d
KuoLiChia_windows <richardkuo1999@gmail.com>
2023-03-19 14:55:02 +0800

    box w*h threshold

--- tools/datasplit/utils/general.py
+++ tools/datasplit/utils/general.py
@@ -29,7 +29,6 @@ def get_bounding_boxes(img):
         # TODO not the best way
         if w*h > 2500:
             boxes.append([x, y, x+w, y+h])
-        boxes.append([x, y, x+w, y+h])
     return np.array(boxes)
 
 
